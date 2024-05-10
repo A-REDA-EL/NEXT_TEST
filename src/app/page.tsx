bb4d4ad1dbdc0aea0f3e702bce93635aadfaf3c0
Ahmed Reda El hili <72277960+Sillco@users.noreply.github.com>
2024-05-10 21:50:03 +0100

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* Your existing JSX code */}
+      <h1>APP ABOUT CAT FACTS :D</h1>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         {/* Display the fetched data */}
         <div>
